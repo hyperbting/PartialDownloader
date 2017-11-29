8159e72b1cbf986c6f6ccb9e10b905866858c4ce
DESKTOP-8R3FD9M\hooloop_PC03 <thomaskao@hooloop.com>
2017-11-29 18:28:29 +0800

    SSL for Unity usage

--- PartialDownload/PartialDownload/Library/WebRequestHelper.cs
+++ PartialDownload/PartialDownload/Library/WebRequestHelper.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 using System.IO;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 
 namespace PartialDownload.Library
 {
@@ -10,6 +12,7 @@ namespace PartialDownload.Library
     {
         public bool CheckServerSupportPartialContent(string _url)
         {
+            ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
             string ContentRanges = "";
             HttpWebResponse resp = null;
 
@@ -54,6 +57,7 @@ namespace PartialDownload.Library
         {
             int ContentLength = -1;
             HttpWebResponse resp = null;
+            ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
 
             try
             {
@@ -92,12 +96,41 @@ namespace PartialDownload.Library
 
         public Stream DownloadParts(string _url, int _start, int _windowSize = 1024)
         {
+            ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
             HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(_url);
             myHttpWebRequest.AddRange(_start, _start + _windowSize - 1);
 
             HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
 
             return myHttpWebResponse.GetResponseStream();
+        }
+
+        public bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            bool isOk = true;
+            // If there are errors in the certificate chain,
+            // look at each error to determine the cause.
+            if (sslPolicyErrors != SslPolicyErrors.None)
+            {
+                for (int i = 0; i < chain.ChainStatus.Length; i++)
+                {
+                    if (chain.ChainStatus[i].Status == X509ChainStatusFlags.RevocationStatusUnknown)
+                    {
+                        continue;
+                    }
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                    chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                    bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                    if (!chainIsValid)
+                    {
+                        isOk = false;
+                        break;
+                    }
+                }
+            }
+            return isOk;
         }
     }
 }
